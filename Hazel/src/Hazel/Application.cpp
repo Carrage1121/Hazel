4af2f8e55d4f65b5e55a59513c71219585baa23f
Carrage1121 <b1329955875@163.com>
2022-12-16 19:06:40 +0800

    Added Hazel key and mouse codes.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,6 +4,7 @@
 #include "Hazel/Log.h"
 
 #include <glad/glad.h>
+#include "Input.h"
 
 
 namespace Hazel
@@ -58,8 +59,8 @@ namespace Hazel
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
 
-			auto [x, y] = Input::GetMousePosition();
-			HZ_CORE_TRACE("{0}, {1}", x, y);
+			//auto [x, y] = Input::GetMousePosition();
+			//HZ_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
